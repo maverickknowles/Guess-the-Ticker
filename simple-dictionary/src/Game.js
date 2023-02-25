5a2cf3f2a7026c027f1c2295966f5c514ee16191
maverickk4 <124333912+maverickk4@users.noreply.github.com>
2023-02-24 21:30:23 -0500

    commit

--- simple-dictionary/src/Game.js
+++ simple-dictionary/src/Game.js
@@ -1,154 +1,221 @@
-
 import React, { useState, useEffect } from 'react';
 import dictionary from './dictionary';
 import './Game.css';
 
-window.onbeforeunload = function() {
-    localStorage.clear();
-  }; 
-
-function Game() {
-  const [mode, setMode] = useState('both');
-  const [answer, setAnswer] = useState('');
-  const [guess, setGuess] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState(60);
+const Game = () => {
+  const [gameMode, setGameMode] = useState('');
+  const [ticker, setTicker] = useState('');
+  const [nameAnswer, setNameAnswer] = useState('');
+  const [companyAnswer, setCompanyAnswer] = useState('');
   const [score, setScore] = useState(0);
-  const [correct, setCorrect] = useState(null);
-  const [inProgress, setInProgress] = useState(false);
-  const [highScore, setHighScore] = useState(localStorage.getItem('highScore') || 0);
-  const [won, setWon] = useState(false);
-  const [initials, setInitials] = useState('');
-  const [enteredInitials, setEnteredInitials] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(30);
+  const [gameStarted, setGameStarted] = useState(false);
 
-  const startGame = () => {
-    setInProgress(true);
-    setScore(0);
-    setAnswer(getRandomKey());
-    setTimeRemaining(60);
-    setGuess('');
-  };
-  
-  const checkGuess = () => {
-    if (!inProgress) {
-      return;
-    }
-    const correctAnswer =
-      mode === 'name' ? dictionary[answer].name.toLowerCase() : answer.toLowerCase();
-    const cleanedGuess = guess.replace(/[\W_]+/g, '').toLowerCase();
-    if (cleanedGuess === correctAnswer) {
-      setScore(score + 1);
-      setCorrect(true);
-    } else {
-      setScore(score - 1);
-      setCorrect(false);
-    }
-    setGuess('');
-    setAnswer(getRandomKey());
-    if (timeRemaining === 0) {
-      setInProgress(false);
-    }
-  };
-  
-
-
-  const getRandomKey = () => {
-    const keys = Object.keys(dictionary);
-    const randomIndex = Math.floor(Math.random() * keys.length);
-    return keys[randomIndex];
-  };
-
-  const handleInitials = (e) => {
-    setInitials(e.target.value.toUpperCase().substr(0, 3));
-  };
-
-  const handleInitialsSubmit = () => {
-    if (score > highScore) {
-      setHighScore(score);
-      localStorage.setItem('highScore', score);
-      if (initials && !enteredInitials) {
-        localStorage.setItem('initials', initials);
-      }
-      
-    }
-    setEnteredInitials(true);
+  const myAlert = (message) => {
+    const alertBox = document.createElement("div");
+    alertBox.className = "my-alert";
+    const alertText = document.createTextNode(message);
+    alertBox.appendChild(alertText);
+    document.body.appendChild(alertBox);
+    setTimeout(() => {
+      alertBox.remove();
+    }, 3000);
   };
   
 
   useEffect(() => {
-    setAnswer(getRandomKey());
+    const intervalId = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
-  useEffect(() => {
-    if (timeRemaining === 0 && !won) {
-      setInProgress(false);
+
+  const startGame = () => {
+    if (!gameMode) {
+      myAlert('Please select a game mode!');
+      return;
     }
-  }, [timeRemaining, won]);
+
+    setGameStarted(true);
+
+    const tickers = Object.keys(dictionary);
+    const randomIndex = Math.floor(Math.random() * tickers.length);
+    const randomTicker = tickers[randomIndex];
+
+    setTicker(randomTicker);
+  };
+
+  const handleNameAnswerChange = (e) => {
+    setNameAnswer(e.target.value);
+  };
+
+  const handleCompanyAnswerChange = (e) => {
+    setCompanyAnswer(e.target.value);
+  };
+
+  const handleNameAnswerSubmit = (e) => {
+    e.preventDefault();
+
+    const correctName = dictionary[ticker].name;
+
+    if (nameAnswer.toLowerCase() === correctName.toLowerCase()) {
+      setScore((prevScore) => prevScore + 1);
+      myAlert('Correct!');
+    } else {
+      myAlert(`Incorrect, the correct answer is "${correctName}".`);
+    }
+
+    setNameAnswer('');
+
+    const tickers = Object.keys(dictionary);
+    const randomIndex = Math.floor(Math.random() * tickers.length);
+    const randomTicker = tickers[randomIndex];
+
+    setTicker(randomTicker);
+  };
+
+  const handleCompanyAnswerSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, company } = dictionary[ticker];
+
+    if (
+      companyAnswer.toLowerCase() === company.toLowerCase()
+    ) {
+      setScore((prevScore) => prevScore + 1);
+      myAlert('Correct!');
+    } else {
+      myAlert(`Incorrect, this is "${name}", and the company is "${company}"`);
+    }
+
+    setNameAnswer('');
+    setCompanyAnswer('');
+
+    const tickers = Object.keys(dictionary);
+    const randomIndex = Math.floor(Math.random() * tickers.length);
+    const randomTicker = tickers[randomIndex];
+
+    setTicker(randomTicker);
+  };
+
+  const handleBothAnswerSubmit = (e) => {
+    e.preventDefault();
+  
+    const { name, company } = dictionary[ticker];
+  
+    if (nameAnswer.toLowerCase() === name.toLowerCase() && companyAnswer.toLowerCase() === company.toLowerCase()) {
+      setScore((prevScore) => prevScore + 1);
+      myAlert('Correct!');
+    } else if (name.toLowerCase() === company.toLowerCase()) {
+      if (nameAnswer.toLowerCase() === name.toLowerCase() || companyAnswer.toLowerCase() === company.toLowerCase()) {
+        setScore((prevScore) => prevScore + 1);
+        myAlert('Correct!');
+      } else {
+        myAlert(`Incorrect, this is "${name}", and the company is "${company}"`);
+      }
+    } else {
+      myAlert(`Incorrect, this is "${name}", and the company is "${company}"`);
+    }
+  
+    setNameAnswer('');
+    setCompanyAnswer('');
+  
+    const tickers = Object.keys(dictionary);
+    const randomIndex = Math.floor(Math.random() * tickers.length);
+    const randomTicker = tickers[randomIndex];
+  
+    setTicker(randomTicker);
+  };
+  
+
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      if (timeRemaining > 0) {
-        setTimeRemaining(timeRemaining - 1);
-      } else {
-        setWon(score >= highScore);
-      }
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [timeRemaining, score, highScore]);
+    const endGame = () => {
+      setGameStarted(false);
+      myAlert(`Congratulations, your score was: ${score}`);
+      setScore(0);
+      setTimeLeft(30);
+      setTicker('');
+      setNameAnswer('');
+      setCompanyAnswer('');
+    };
+  
+    if (timeLeft === 0) {
+      endGame();
+    }
+  }, [timeLeft, score, setGameStarted, setTimeLeft, setTicker, setNameAnswer, setCompanyAnswer]);
+  
 
-  return (
-    <div className="container">
-      <h1 className="title">Guess the Coin</h1>
-      <div className="buttons">
-        <button className={`btn ${mode === 'name' ? 'active' : ''}`} onClick={() => setMode('name')}>Name</button>
-        <button className={`btn ${mode === 'company' ? 'active' : ''}`} onClick={() => setMode('company')}>Company</button>
-        <button className={`btn ${mode === 'both' ? 'active' : ''}`} onClick={() => setMode('both')}>Both</button>
-      </div>
-
-
-  {inProgress ? (
-    <div className="game">
-      <div className="timer">Time remaining: {timeRemaining}</div>
-      <div className="score">Score: {score}</div>
-      <div className="question">{mode === 'name' ? answer : dictionary[answer][mode]}</div>
-      <div className={`message ${correct === true ? 'correct' : 'incorrect'}`}>
-        {correct === true && <div>Correct!</div>}
-        {correct === false && <div>Incorrect. Try again!</div>}
-      </div>
-      
-      {inProgress && timeRemaining > 0 && (
-        <>
-        <input className="guess" type="text" value={guess} onChange={e => setGuess(e.target.value)} onKeyPress={(e) => {
-        if (e.key === 'Enter') {
-          checkGuess();
-        }
-      }} />
-        <button className="submit-btn" onClick={checkGuess}>Submit</button>
-        </>
-      )}
-
-      {timeRemaining === 0 && score >= highScore && won && (
-        <div className="win">
-          <div>Congratulations, you won!</div>
-          <div>Please enter your initials:</div>
-          <input className="initials" type="text" maxLength="3" onChange={handleInitials} value={initials} />
-          <button className="submit-initials" onClick={handleInitialsSubmit}>Submit</button>
-          <button className="reset-btn" onClick={() => {
-            setInProgress(false);
-            setTimeRemaining(60);
-            setScore(0);
-            setInitials(initials);
-          }}>Reset Timer</button>
+  const renderGameModeSelect = () => {
+    return (
+      <>
+        <h1>Guess the Coin</h1>
+        <div className="mode-buttons">
+          <button
+            className={gameMode === 'name' ? 'selected' : ''}
+            onClick={() => setGameMode('name')}
+          >
+            Name
+          </button>
+          <button
+            className={gameMode === 'company' ? 'selected' : ''}
+            onClick={() => setGameMode('company')}
+          >
+            Company
+          </button>
+          <button
+            className={gameMode === 'both' ? 'selected' : ''}
+            onClick={() => setGameMode('both')}
+          >
+            Both
+          </button>
         </div>
-      )}
+        <br />
+        <button onClick={startGame}>Start</button>
+      </>
+    );
+  };
+  
 
-    </div>
-  ) : (
-    <button className="start-btn" onClick={startGame}>Start</button>
-  )}
-  <div className="high-score">High Score: {initials} - {highScore}</div>
+  const renderGame = () => {
+    return (
+      <>
+        <h1>Guess the Coin</h1>
+        {gameMode === 'name' ? (
+          <>
+            <h2>{dictionary[ticker].symbol}</h2>
+            <form onSubmit={handleNameAnswerSubmit}>
+              <input type="text" placeholder="Name" value={nameAnswer} onChange={handleNameAnswerChange} />
+              <button type="submit">Submit</button>
+            </form>
+          </>
+        ) : gameMode === 'company' ? (
+          <>
+            <h2>{dictionary[ticker].symbol}</h2>
+            <form onSubmit={handleCompanyAnswerSubmit}>
+              <input type="text" placeholder="Company" value={companyAnswer} onChange={handleCompanyAnswerChange} />
+              <button type="submit">Submit</button>
+            </form>
+          </>
+        ) : gameMode === 'both' ? (
+          <>
+            <h2>{dictionary[ticker].symbol}</h2>
+            <form onSubmit={handleBothAnswerSubmit}>
+              <input type="text" placeholder="Name" value={nameAnswer} onChange={handleNameAnswerChange} />
+              <input type="text" placeholder="Company" value={companyAnswer} onChange={handleCompanyAnswerChange} />
+              <button type="submit">Submit</button>
+            </form>
+          </>
+        ) : null}
+        <p>Score: {score}</p>
+        <p>Time Left: {timeLeft}</p>
+      </>
+    );
+  };
 
-</div>
-);
-}
+return gameStarted ? renderGame() : renderGameModeSelect();
+};
 
 export default Game;
